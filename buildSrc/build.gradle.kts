4fe8f4e19eada536a306a68f3b4d41f719ceecbc
rahul07dev <rahulsureshpawar04@gmail.com>
2025-07-12 19:00:03 +0530

    Fix Gradle Plugin Version Conflict

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,6 +9,6 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:8.11.0")
+    implementation("com.android.tools.build:gradle:8.7.3")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
 }
